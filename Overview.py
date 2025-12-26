a76fcaca00eeb76c4b6ccccf218ba2333cf555b1
OTAKYHUH <ahguamanturngay@gmail.com>
2025-12-26 17:03:55 +0800

    pls

--- Overview.py
+++ Overview.py
@@ -29,4 +29,4 @@ def open_browser():
 if __name__ == '__main__':
     # Launch browser in a separate thread
     threading.Timer(1.5, open_browser).start()
-    app.run(debug=False)
+    app.run(debug=True)
